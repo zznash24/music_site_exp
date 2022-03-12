8a1b7684503f541c861579a2e31d6f378147b0d0
Zach Nash <afreakenturtle@gmail.com>
2022-03-12 15:31:51 -0500

    music commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const songData = require('./song');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+app.get('/:artist', (req, res) => {
+    let artist = songData.find((artist) => artist.name === artist.name);
+
+    res.json(artist)
+})
+
+
+
+app.listen(3000, () => {
+    console.log('DoomsDay Device is Active')
+})
---
+++ song.js
@@ -0,0 +1,35 @@
+let songs = [
+{
+    name:"The Carter",
+    publishDate:"2004",
+    imgURL:"images/theCarter.jpg",
+
+   songTitles:["song1","song2", "song3", "song4"]
+},
+
+{  
+    name:"Free Weezy",
+    description:"2015",
+    imgURL:"images/FreeWeezy.jpg",
+
+   songTitles:["song1","song2", "song3", "song4"]
+},
+
+{
+    name:"The Eminem Show",
+    publishDate:"2002",
+    imgURL:"https://upload.wikimedia.org/wikipedia/en/thumb/3/35/The_Eminem_Show.jpg/220px-The_Eminem_Show.jpg/theCarter.jpg",
+
+   songTitles:["song1","song2", "song3", "song4"]
+},
+
+{  
+    name:"Kamikaze",
+    description:"2018",
+    imgURL:"https://upload.wikimedia.org/wikipedia/en/thumb/6/62/Eminem_-_Kamikaze.jpg/220px-Eminem_-_Kamikaze.jpg",
+
+   songTitles:["song1","song2", "song3", "song4"]
+}
+]
+
+module.exports = songData;
